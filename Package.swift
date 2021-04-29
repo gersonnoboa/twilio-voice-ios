c269fc85c0fc52f42a3ef0a037cf1151d51100c2
Gerson Noboa <gerson.noboa@glia.com>
2021-04-29 18:24:52 +0300

    Changed

--- Package.swift
+++ Package.swift
@@ -4,19 +4,25 @@ import PackageDescription
 let package = Package(
     name: "TwilioVoice",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
         .library(
             name: "TwilioVoice",
             type: .dynamic,
-            targets: ["TwilioVoice"]),
+            targets: ["TwilioVoiceTarget"]),
     ],
     targets: [
         .binaryTarget(
             name: "TwilioVoice",
             url: "https://github.com/twilio/twilio-voice-ios/releases/download/6.2.2/TwilioVoice.xcframework.zip",
             checksum: "e8411732eff2ad79bcba744385b402523f2bddc9cb9c8654faa27b62dc9a12ec"
+        )
+        .target(
+            name: "TwilioVoiceTarget",
+            dependencies: [
+                "TwilioVoice"
+            ]
         )
     ]
 )
