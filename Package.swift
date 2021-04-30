c787ff9021d4de277eaa60f17e244bf2707a26f3
Gerson Noboa <gerson.noboa@glia.com>
2021-04-30 07:50:47 +0300

    Updates

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     products: [
         .library(
             name: "TwilioVoice",
-            type: .dynamic,
             targets: ["TwilioVoiceTarget"]),
     ],
     targets: [
